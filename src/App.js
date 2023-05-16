f20f5835e94187931d1541ff42936a3276ef7c73
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-05-16 13:44:29 +0530

    Added Function for fetching data from api

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useState, useEffect} from 'react';
+
+
+function App() {
+  const [data,setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/manas1410`)
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+  if (data) return (<pre>{JSON.stringify(data, null, 2)}</pre>);
+  return <h1>Data</h1>;
+}
+
+export default App;
