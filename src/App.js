a7acc3d8401630154b6fcadc6bd915b27f8a173a
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-05-16 13:54:21 +0530

    Showing only name, username and profile pic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 import { useState, useEffect} from 'react';
 
+function GithubUser({name,username, avatar}){
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{username}</p>
+      <img src={avatar} height={150} alt = {name}/>
+    </div>
+  )
+}
 
 function App() {
   const [data,setData] = useState(null);
@@ -9,7 +18,7 @@ function App() {
       .then((response) => response.json())
       .then(setData);
   }, []);
-  if (data) return (<pre>{JSON.stringify(data, null, 2)}</pre>);
+  if (data) return (<GithubUser name={data.name} username={data.login} avatar={data.avatar_url}/>);
   return <h1>Data</h1>;
 }
 
