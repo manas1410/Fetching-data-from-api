1319a39fe210ab86701d5c8e39858b58c074f48b
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2023-05-17 17:24:50 +0530

    Loading and Error state added

--- src/App.js
+++ src/App.js
@@ -13,13 +13,21 @@ function GithubUser({name,username, avatar}){
 
 function App() {
   const [data,setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/manas1410`)
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
-  if (data) return (<GithubUser name={data.name} username={data.login} avatar={data.avatar_url}/>);
-  return <h1>Data</h1>;
+  if(loading) return <h1>Loading</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if(!data) return null;
+  return (<GithubUser name={data.name} username={data.login} avatar={data.avatar_url}/>);
 }
 
 export default App;
